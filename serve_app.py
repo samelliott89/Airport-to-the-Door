38d4b69fd4f473ffa059f45ab8f96905a901658b
Samuel Killin <sam@canva.com>
2015-11-28 13:18:53 +1100

    two sp. indent. add root route.

--- serve_app.py
+++ serve_app.py
@@ -3,8 +3,8 @@
 import sys
 
 if len(sys.argv) < 2:
-    print "Usage: python serve_app.py <relative path to static assets>"
-    sys.exit()
+  print "Usage: python serve_app.py <relative path to static assets>"
+  sys.exit()
 
 from flask import Flask, request, send_from_directory, Response
 import requests
@@ -16,52 +16,56 @@ _BASE_URL = 'http://localhost:8090'
 flask_app = Flask(__name__)
 
 def build_proxy_request(method):
-    if method == 'GET':
-        return requests.get
-    if method == 'POST':
-        return requests.post
-    if method == 'PUT':
-        return requests.put
-    if method == 'DELETE':
-        return requests.delete
-    if method == 'OPTIONS':
-        return requests.options
+  if method == 'GET':
+    return requests.get
+  if method == 'POST':
+    return requests.post
+  if method == 'PUT':
+    return requests.put
+  if method == 'DELETE':
+    return requests.delete
+  if method == 'OPTIONS':
+    return requests.options
 
 @flask_app.route('/query/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'])
 def reverse_proxy(path):
-    try:
-        # build request components
-        method = request.method
-        url = _BASE_URL + '/' + path
-        headers = { each[0] : each[1] for each in request.headers }
-        body = request.data
-        query_params = { param : request.args.get(param) for param in request.args }
+  try:
+    # build request components
+    method = request.method
+    url = _BASE_URL + '/' + path
+    headers = { each[0] : each[1] for each in request.headers }
+    body = request.data
+    query_params = { param : request.args.get(param) for param in request.args }
 
-        # make request to service
-        proxy_req = build_proxy_request(method)
-        proxy_res = proxy_req(url, data=body, headers=headers, params=query_params)
+    # make request to service
+    proxy_req = build_proxy_request(method)
+    proxy_res = proxy_req(url, data=body, headers=headers, params=query_params)
 
-        # parse service response
-        status = proxy_res.status_code
-        body = proxy_res.text
-        headers = { param : proxy_res.headers.get(param) for param in proxy_res.headers }
+    # parse service response
+    status = proxy_res.status_code
+    body = proxy_res.text
+    headers = { param : proxy_res.headers.get(param) for param in proxy_res.headers }
 
-        # form response to client
-        res = Response(body)
-        res.status_code = status
-        res.headers = headers
-        return res
-    except Exception as e:
-        print(traceback.format_exc())
-        return str(e), 500
+    # form response to client
+    res = Response(body)
+    res.status_code = status
+    res.headers = headers
+    return res
+  except Exception as e:
+    print(traceback.format_exc())
+    return str(e), 500
 
 @flask_app.route('/<path:filename>')
 def static_assets(filename):
-    try:
-        return send_from_directory(_STATIC_ASSET_PATH, filename)
-    except Exception as e:
-        print(traceback.format_exc())
-        return str(e), 500
+  try:
+    return send_from_directory(_STATIC_ASSET_PATH, filename)
+  except Exception as e:
+    print(traceback.format_exc())
+    return str(e), 500
+
+@flask_app.route('/')
+def root():
+  return send_from_directory(_STATIC_ASSET_PATH, 'index.html')
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+  flask_app.run(debug=True)
